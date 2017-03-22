1121fae3e9b92b08f7f59a345933e802db425248
Ben C <ben.cuan@gmail.com>
2017-03-21 19:26:22 -0700

    Add Camera Scrolling and Minion Turning
    Getting so close to MVP release!

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 ///Attach to camera to allow screen to move with mouse.
 public class CameraMove : MonoBehaviour {
 
+	Camera theCamera;
+	float fov;
 	public const float SCROLL_SPEED = 200f;
+	public const float MOUSE_SCROLL_SPEED = 8f;
 
 	// Use this for initialization
 	void Start () {
-		
+		theCamera = GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
@@ -22,5 +25,13 @@ public class CameraMove : MonoBehaviour {
 			 transform.Translate(Vector3.right * Time.deltaTime * SCROLL_SPEED, Space.World);
 		else if (Input.mousePosition.x <= Screen.width*0.05) 
 			 transform.Translate(Vector3.left * Time.deltaTime * SCROLL_SPEED, Space.World);
+
+		fov = theCamera.fieldOfView;
+
+		if (Input.GetAxis("Mouse ScrollWheel") < 0 && fov < 60)
+			theCamera.fieldOfView += MOUSE_SCROLL_SPEED;
+        	
+     	if (Input.GetAxis("Mouse ScrollWheel") > 0 && fov > 20)
+			 theCamera.fieldOfView -= MOUSE_SCROLL_SPEED;
 	}
 }
--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Minion : MonoBehaviour {
 
-	Transform trans;
 	CharacterController controller;
 	MinionState currState;
 
 	public const float SPEED = 0.5f;
 
+	public int mX, mY;
+	public Vars.Rotation currRotation;
 
 	///Someone PLEASE make an animation controller with this 
 	public enum MinionState {
@@ -18,27 +19,51 @@ public class Minion : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		trans = GetComponent<Transform> ();
 		controller = GetComponent<CharacterController> ();
 		currState = MinionState.Forward;
+		currRotation = Vars.Rotation.forward;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(currState == MinionState.Forward)
-			trans.Translate(new Vector3(0f, SPEED, 0f));
+		if(currState == MinionState.Forward) {
+			
+			switch(currRotation) {
+				case Vars.Rotation.forward:
+					mX = 1;
+					mY = 0;
+					break;
+				case Vars.Rotation.back:
+					mX = -1;
+					mY = 0;
+					break;
+				case Vars.Rotation.left:
+					mX = 0;
+					mY = -1;
+					break;
+				case Vars.Rotation.right:
+					mX = 0;
+					mY = 1;
+					break;
+			}
+				
+		}
+			transform.Translate(new Vector2(mX,mY));
 	}
 
-	void OnTriggerEnter (Collider col) {
+	void OnTriggerEnter2D (Collider2D col) {
 		//3 types of triggers: turn left, turn right, go to nearest tower.
         //...
+
+		Debug.Log("Collide");
+
 		switch(col.tag) {
 
 			case "TurnLeft":
-				trans.Rotate(Vector3.left);
+				currRotation = (Vars.Rotation)((int)(currRotation) + 1);
 				break;
 			case "TurnRight":
-				trans.Rotate(Vector3.right);
+				currRotation = (Vars.Rotation)((int)(currRotation) - 1);
 				break;
 			case "GotoTower":
 				currState = MinionState.ToTower;
