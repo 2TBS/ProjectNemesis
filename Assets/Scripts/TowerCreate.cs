0ca7b9fe59425c2bbf4cf403eee20d94b365e7ba
Ben Cuan <ben.cuan@gmail.com>
2017-05-30 15:44:38 -0700

    Add TowerCreateButton
    Not yet working. Just creates an image of the tower when clicked.

---
+++ Assets/Scripts/TowerCreate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+///Logic for the creation of towers. Attach to TowerCreateButton.
+public class TowerCreate : MonoBehaviour {
+
+	public Image towerImage; //Which tower is this?
+	public Image followMouseSprite; //follows the mouse until put down
+	public bool placing; //Is the player currently placing a tower?
+
+	// Use this for initialization
+	void Start () {
+		followMouseSprite.sprite = towerImage.sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(placing) {
+			followMouseSprite.transform.position = Input.mousePosition;
+			if(Input.GetMouseButtonDown(0))
+		}
+	}
+
+	///Add to OnClickEvent for TowerCreateButton
+	public void clickAction () {
+		placing = !placing;
+
+		followMouseSprite.enabled = placing;
+	}
+}
