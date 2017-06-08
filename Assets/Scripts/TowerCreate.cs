9a029a188b33581a6e86cdaa04e4e08345a34e55
Ben Cuan <ben.cuan@gmail.com>
2017-06-08 12:08:58 -0700

    Fix Darkrift errors
    Some files were missing

--- Assets/Scripts/TowerCreate.cs
+++ Assets/Scripts/TowerCreate.cs
@@ -8,28 +8,48 @@ public class TowerCreate : MonoBehaviour {
 
 	public Image towerImage; //Which tower is this?
 	public Image followMouseSprite; //follows the mouse until put down
+	public Sprite blankImage;
+	public Object towerPrefab;
 	public bool placing; //Is the player currently placing a tower?
 
 	// Use this for initialization
 	void Start () {
-		followMouseSprite.sprite = towerImage.sprite;
+		//followMouseSprite.sprite = towerImage.sprite;
+		followMouseSprite.transform.position = new Vector3(100000000, 0, 0); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(placing) {
+		
 			followMouseSprite.transform.position = Input.mousePosition;
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+			//if(Input.GetMouseButtonDown(0))
+=======
+=======
+>>>>>>> d498cabc876474023394e41717f54b3894a8aae9
+=======
+>>>>>>> origin/develop
             if (Input.GetMouseButtonDown(0))
             {
 
             }
 		}
+>>>>>>> origin/develop
 	}
 
 	///Add to OnClickEvent for TowerCreateButton
 	public void clickAction () {
 		placing = !placing;
-
-		followMouseSprite.enabled = placing;
+		if (placing) {
+			var v3 = Input.mousePosition;
+			v3.z = 10.0f;
+			//v3 = Camera.main.ScreenToWorldPoint (v3);
+			GameObject tower = Instantiate (towerPrefab, v3, Quaternion.identity) as GameObject;
+			followMouseSprite.sprite = blankImage;
+		} else {
+			followMouseSprite.sprite = towerImage.sprite;
+		}
 	}
 }
