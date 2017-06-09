e00c709aaf6f3a745c6f513c6fe142d3568625aa
Omar Hossain <omarepic1@gmail.com>
2017-06-09 10:36:39 -0700

    Fixed Minion Rect Transform Destroyed Issue

--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -14,7 +14,7 @@ public class Minion : MonoBehaviour {
 
 	///Someone PLEASE make an animation controller with this 
 	public enum MinionState {
-		Stopped, Forward, ToTower, Fighting
+		Stopped, Forward, ToTower, Fighting, Dead
 	}
 
 	// Use this for initialization
@@ -26,28 +26,30 @@ public class Minion : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(currState == MinionState.Forward) {
+		if(currState == MinionState.Dead) {
+			Destroy(gameObject);
+			return;
+		} else if (currState == MinionState.Forward) {
 			
-			switch(currRotation) {
-				case Vars.Rotation.forward:
-					mX = 1;
-					mY = 0;
-					break;
-				case Vars.Rotation.back:
-					mX = -1;
-					mY = 0;
-					break;
-				case Vars.Rotation.left:
-					mX = 0;
-					mY = -1;
-					break;
-				case Vars.Rotation.right:
-					mX = 0;
-					mY = 1;
-					break;
-			}
-				
-		}
+			switch (currRotation) {
+			case Vars.Rotation.forward:
+				mX = 1;
+				mY = 0;
+				break;
+			case Vars.Rotation.back:
+				mX = -1;
+				mY = 0;
+				break;
+			case Vars.Rotation.left:
+				mX = 0;
+				mY = -1;
+				break;
+			case Vars.Rotation.right:
+				mX = 0;
+				mY = 1;
+				break;
+			} 
+		} 
 			transform.Translate(new Vector2(mX,mY));
 	}
 
@@ -69,7 +71,7 @@ public class Minion : MonoBehaviour {
 				currState = MinionState.ToTower;
 				break;
 			case "Destroy":
-				Destroy(gameObject);
+				currState = MinionState.Dead;
 				break;
 		}
 	}
